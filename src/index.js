8ec681e7c30faecf93ad2c59059a83b55f834c63
Kirill Shevtsov <kirwork.em@gmail.com>
2020-08-24 20:46:07 +0300

    Refactoring: modifying content render method

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import model from './model';
-import blockTemplates from './block-templates';
 import './styles/main.css';
 
 const sitePreview = document.getElementById('site-preview');
 
 model.forEach((blockElement) => {
-  const content = blockTemplates[blockElement.blockCreatorFunctionName];
-
-  if (content) {
-    const html = content(blockElement);
-    sitePreview.insertAdjacentHTML('beforeend', html);
-  }
+  sitePreview.insertAdjacentHTML('beforeend', blockElement.toHTML());
 });
